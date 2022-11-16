cb2c75cc7d715d74619275edfef975c316d517ea
yeXela13 <103512230+yeXela13@users.noreply.github.com>
2022-11-16 19:53:36 +0300

    загрузить popap

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const popap = document.querySelector('.popap');
+const editButton = document.querySelector('.profile__edit-button');
+const likeButton = document.querySelector('.element__like');
+
+editButton.addEventListener('click')
+popap.addEventListener
+popap.setAttribute('display', 'block');
