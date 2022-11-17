b131065583e823510578cf9419d82fa119cfe7bd
Alexey Punin <Alexey13.90@mail.ru>
2022-11-17 09:42:57 +0300

    Загрузить Form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,33 @@
-const popap = document.querySelector('.popap');
-const editButton = document.querySelector('.profile__edit-button');
-const likeButton = document.querySelector('.element__like');
+const popapElement = document.querySelector('.popap');
+const popapCloseElement = popapElement.querySelector('.popap__close');
+const popapOpenElement = document.querySelector('.profile__edit-button');
 
-editButton.addEventListener('click')
-popap.addEventListener
-popap.setAttribute('display', 'block');
+const togglePopapVisibility = function (event) {
+    popapElement.classList.toggle('popap__opened');
+};
+
+popapOpenElement.addEventListener('click', togglePopapVisibility);
+popapCloseElement.addEventListener('click', togglePopapVisibility);
+
+// Находим форму в DOM
+const formElement = popapElement.querySelector('.form'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = formElement.querySelector('.profile__name');// Воспользуйтесь инструментом .querySelector()
+const jobInput = formElement.querySelector('.profile__post');// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    nameInput
+    jobInput
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
