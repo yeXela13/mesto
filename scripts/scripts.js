2916de20f1c24cb9c4b5f6d627ebb86017434c65
yeXela13 <103512230+yeXela13@users.noreply.github.com>
2022-11-19 19:38:25 +0300

    загрузить bag-fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,28 +2,29 @@ const popapElement = document.querySelector('.popap');
 const popapCloseElement = popapElement.querySelector('.popap__close');
 const popapOpenElement = document.querySelector('.profile__edit-button');
 const popapSaveButton = popapElement.querySelector('.form__save');
-
-
-
-const togglePopapVisibility = function (event) {
-    popapElement.classList.toggle('popap__opened');
-    nameInput.value = profileName.textContent;
-    postInput.value = profilePost.textContent;
-};
-
-popapOpenElement.addEventListener('click', togglePopapVisibility);
-popapCloseElement.addEventListener('click', togglePopapVisibility);
-
 const formElement = popapElement.querySelector('.form');
 const nameInput = formElement.querySelector('.form__textarea_profile_name');
 const postInput = formElement.querySelector('.form__textarea_profile_post');
 const profileName = document.querySelector('.profile__name');
 const profilePost = document.querySelector('.profile__post');
 
+const addPopapVisibility = function (event) {
+    popapElement.classList.add('popap_opened');
+    nameInput.value = profileName.textContent;
+    postInput.value = profilePost.textContent;
+};
+
+const removePopapVisibility = function (event) {
+    popapElement.classList.remove('popap_opened');
+};
+
+popapOpenElement.addEventListener('click', addPopapVisibility);
+popapCloseElement.addEventListener('click', removePopapVisibility);
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profilePost.textContent = postInput.value;
-    togglePopapVisibility();
+    removePopapVisibility();
 }
 formElement.addEventListener('submit', formSubmitHandler); 
