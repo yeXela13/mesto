dff63cc75d81129683be3105ec604fbe541398c5
Alexey Punin <Alexey13.90@mail.ru>
2022-12-01 08:56:48 +0300

    Загрузить Удаление

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -35,8 +35,8 @@ const popapItemClosebutton = popapItemElement.querySelector('.popap-item__close'
 const popapItemOpenElement = document.querySelector('.profile__add-button');
 const popapItemCreateButton = popapItemElement.querySelector('.form-item__create');
 const formItemElement = popapItemElement.querySelector('.form-item');
-const placeNameInput = formItemElement.querySelector('.form-item__textarea_element_name');
-const placeUrlInput = formItemElement.querySelector('.form-item__textarea_element_url');
+const placeElementText = document.querySelector('.element__text');
+const placeElementLink = document.querySelector('.element__image');
 const elementName = document.querySelector('.form-item__textarea_element_name');
 const elementUrl = document.querySelector('.form-item__textarea_element_url');
 const initialCards = [
@@ -47,16 +47,16 @@ const initialCards = [
     {
         name: 'Мальдивы',
         link: 'https://images.unsplash.com/photo-1669181310799-fc929c93ed58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80'
-    },  {
+    }, {
         name: 'Турция',
         link: 'https://images.unsplash.com/photo-1669111957726-9c2f6aec0199?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },  {
+    }, {
         name: 'Малайзия',
         link: 'https://images.unsplash.com/photo-1668694504921-5bcf11d34eef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },  {
+    }, {
         name: 'Аргентина',
         link: 'https://images.unsplash.com/photo-1668293498006-cf8ad7ef2470?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
-    },  {
+    }, {
         name: 'Грузия',
         link: 'https://images.unsplash.com/photo-1668368047837-3d9c67145679?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
     }
@@ -64,32 +64,46 @@ const initialCards = [
 //Открыть/закрыть
 const addPopapItemVisibility = function (event) {
     popapItemElement.classList.add('popap-item_opened');
+    console.log('Открыть');
 }
 const removePopapItemVisibility = function (event) {
     popapItemElement.classList.remove('popap-item_opened');
+    console.log('Закрыть');
 }
 popapItemOpenElement.addEventListener('click', addPopapItemVisibility);
 popapItemClosebutton.addEventListener('click', removePopapItemVisibility);
 //шаблон
 const cardTemplate = document.querySelector('#element-template').content.querySelector('.element__item');
 //генерация карточки
+const handleDeleteCard = (event) => {
+    event.target.closest('.element__item').remove();
+};
+const handleLikeCard = (event) => {
+    event.target.classList.toggle('element__like-active');
+};
 const generateCard = (item) => {
-const newCard = cardTemplate.cloneNode(true);
-const name = newCard.querySelector('.element__text');
-const link = newCard.querySelector('.element__image')
-name.textContent = item.name;
-link.src = item.link;
-link.alt = item.name;
-return newCard;
-}
+    const newCard = cardTemplate.cloneNode(true);
+    const name = newCard.querySelector('.element__text');
+    name.textContent = item.name;
+    const link = newCard.querySelector('.element__image')
+    link.src = item.link;
+    link.alt = item.name;
+    const deleteItemElement = newCard.querySelector('.element__delete-button');
+    deleteItemElement.addEventListener('click', handleDeleteCard);
+    const likeElement = newCard.querySelector('.element__like');
+    likeElement.addEventListener('click', handleLikeCard);
+    return newCard;
+};
 //обработчик
-function formElementSubmitHandler(evt) {
-    evt.preventDefault();
-    renderCard({name: input.value})
-    input.value = '';
-    removePopapVisibility();
-}
-formItemElement.addEventListener('submit', formElementSubmitHandler);
+function handleFormElementSubmit(event) {
+    event.preventDefault();
+
+
+    
+    console.log('проверка связи');
+    removePopapItemVisibility();
+};
+formItemElement.addEventListener('submit', handleFormElementSubmit);
 
 //добавить карточки
 const renderCard = (item) => {
@@ -98,18 +112,4 @@ const renderCard = (item) => {
 
 initialCards.forEach((item) => {
     renderCard(item);
-    });
-
-//удаление карточки
-const deleteItemElementButton = document.querySelector('.element__delete-button');
-const deleteItemElement = function (event) {
-initialCards.shift();
-}
-deleteItemElementButton.addEventListener('click', deleteItemElement);
-
-//Лайк
-const likeElement = document.querySelector('.element__like');
-function likeElementClick(event) {
-    likeElement.classList.toggle('element__like-active');
-}
-likeElement.addEventListener('click', likeElementClick);
+});
