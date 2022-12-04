a7e3512f787bec8f1d25879025353bf23eeaeef0
Alexey Punin <Alexey13.90@mail.ru>
2022-12-04 09:48:00 +0300

    Загрузить Popap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,9 +19,10 @@ const initialCards = [
         name: 'Грузия',
         link: 'https://images.unsplash.com/photo-1668368047837-3d9c67145679?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
     }
-];
+]
 const popapElement = document.querySelector('.popap');
-const popapCloseElement = popapElement.querySelector('.popap__close');
+const popapEditElement = document.querySelector('.popap_edit-card');
+const popapCloseElement = document.querySelector('.popap__close');
 const popapOpenElement = document.querySelector('.profile__edit-button');
 const popapSaveButton = popapElement.querySelector('.form__save');
 const formElement = popapElement.querySelector('.form');
@@ -29,18 +30,39 @@ const nameInput = formElement.querySelector('.form__textarea_profile_name');
 const postInput = formElement.querySelector('.form__textarea_profile_post');
 const profileName = document.querySelector('.profile__name');
 const profilePost = document.querySelector('.profile__post');
-const addPopapVisibility = function (event) {
+const elementList = document.querySelector('.element');
+const popapItemElement = document.querySelector('.popap_add-card');
+const popapItemOpenElement = document.querySelector('.profile__add-button');
+const popapItemCreateButton = popapItemElement.querySelector('.form-item__create');
+const formItemElement = popapItemElement.querySelector('.form-item');
+const placeElementText = document.querySelector('.element__text');
+const placeElementLink = document.querySelector('.element__image');
+const elementName = document.querySelector('.form-item__textarea_element_name');
+const elementUrl = document.querySelector('.form-item__textarea_element_url');
+const cardTemplate = document.querySelector('#element-template').content.querySelector('.element__item');
+const popapOpenCardElement = document.querySelector('.popap_open-card');
+const popapImage = document.querySelector('.popap__image');
+const popapCaption = document.querySelector('.popap__caption');
+
+function addPopapVisibility() {
     popapElement.classList.add('popap_opened');
-    // nameInput.value = profileName.textContent;
-    // postInput.value = profilePost.textContent;
-    console.log('Открыть');
 };
-const removePopapVisibility = function (event) {
+function removePopapVisibility() {
     popapElement.classList.remove('popap_opened');
-    console.log('Закрыть');
 };
-popapOpenElement.addEventListener('click', addPopapVisibility);
+function saveValues() {
+    nameInput.value = profileName.textContent;
+    postInput.value = profilePost.textContent;
+};
+popapOpenElement.addEventListener('click', function addPopapEditVisibility() {
+    addPopapVisibility (popapEditElement);
+    saveValues();
+});
+popapItemOpenElement.addEventListener('click', function openAddPopapElement() {
+    addPopapVisibility(popapItemElement);
+});
 popapCloseElement.addEventListener('click', removePopapVisibility);
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
@@ -48,62 +70,44 @@ function formSubmitHandler(evt) {
     removePopapVisibility();
 }
 formElement.addEventListener('submit', formSubmitHandler);
-//Popap-add
-
-const OpenAddCardPopap = document.querySelector('.profile__add-button');
-const elementList = document.querySelector('.element__item');
-const elementTemplate = document.querySelector('#element-template').content.querySelector('.element__item');
-const popapImage = document.querySelector('.popap__image');
-const popapCaption = document.querySelector('.popap__caption');
-const renderCard = (item) => {
-    elementList.prepend(createElement(item));
+//PopapItem
+const handleDeleteCard = (event) => {
+    event.target.closest('.element__item').remove();
 };
-
-const handleLikeButton = (e) => {
-e.target.closest('.element__item').classList.toggle('element__like-active');
+const handleLikeCard = (event) => {
+    event.target.classList.toggle('element__like-active');
 };
-const handleDeleteButton = (e) => {
-    e.target.closest('.element__item').remove();
-};
-
-const formCreate = document.querySelector('.form-item');
-const formCreateName = document.querySelector('.form-item__textarea_element_name');
-const formCreateLink = document.querySelector('.form-item__textarea_element_url ');
-
-const handleFormCreateSubmit = (e) => {
-    e.preventDefault();
-    renderCard({
-        name: formCreateName.value,
-        link: formCreateLink.value
-    });
-    renderCard = ' ';
-    removePopapVisibility();
-    console.log('Закрыть');
-};
-function createElement(item) {
-    const newCard = elementTemplate.cloneNode(true);
-    const elementTemplateName = newCard.querySelector('.element__text');
-    elementTemplateName.textContent = item.name;
-    const elementTemplateLink = newCard.querySelector('.element__image');
-    elementTemplateLink.src = item.link;
-    elementTemplateLink.alt = item.name;
-
-    // const likeCard = newCard.querySelector('.element__like');
-    // const deleteCard = newCard.querySelector('.element__delete-button');
-    // likeCard.addEventListener('click', handleLikeButton);
-    // deleteCard.addEventListener('click', handleDeleteButton);
-    console.log('проверка');
+const openCardPopap = function (event) {
+    popapImage.src = event.target.curentSrc;
+    popapImage.alt = event.target.curentAlt;
+    popapCaption.textContent = event.target.innerText;
+    addPopapVisibility(popapOpenCardElement);
+    };
+const generateCard = (item) => {
+    const newCard = cardTemplate.cloneNode(true);
+    const name = newCard.querySelector('.element__text');
+    name.textContent = item.name;
+    const link = newCard.querySelector('.element__image')
+    link.src = item.link;
+    link.alt = item.name;
+    const deleteItemElement = newCard.querySelector('.element__delete-button');
+    deleteItemElement.addEventListener('click', handleDeleteCard);
+    const likeElement = newCard.querySelector('.element__like');
+    likeElement.addEventListener('click', handleLikeCard);
+    link.addEventListener('click', openCardPopap);
     return newCard;
 };
+function handleFormElementSubmit(event) {
+    event.preventDefault();
+    renderCard({ name: elementName.value, link: elementUrl.value });
+    renderCard.value = '';
+    removePopapVisibility(popapItemElement);
+};
+popapItemCreateButton.addEventListener('click', handleFormElementSubmit);
 
-initialCards.forEach(function (item) {
-    const elementCard = createElement(item);
-elementList.append(elementCard);
+const renderCard = (item) => {
+    elementList.prepend(generateCard(item));
+};
+initialCards.forEach((item) => {
+    renderCard(item);
 });
-//Слушатель
-formCreate.addEventListener('submit', handleFormCreateSubmit);
-
-// const openCardPopap (e) => {
-//     popapImage.src = e.target
-//     popapCaption.textContent = 
-// };
